98e112869b4f0cd895a45178191e90db658ec665
farukipekcom <faruk@farukipek.com>
2023-08-20 19:04:04 -0400

    styles: update footer text

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function Footer() {
   return (
     <div className="flex h-20 items-center border-t border-solid border-borderLight  dark:border-border">
-      <div className="w-full border-r border-solid border-borderLight p-6  text-sm font-medium text-white dark:border-border">
+      <div className="w-full border-r border-solid border-borderLight p-6  text-sm font-medium text-primary dark:border-border dark:text-white">
         © 2023 Made with ❤️ and ☕ in Charlotte.
       </div>
     </div>
