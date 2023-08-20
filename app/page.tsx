b5f80a467ded1b09c9352de43b45af102747e8cf
farukipekcom <faruk@farukipek.com>
2023-08-20 19:03:29 -0400

    feat: add homepage card

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,16 @@
+import Card from "./components/card";
 export default function Home() {
-  return <main className=""></main>;
+  return (
+    <main className="p-6">
+      <div className=" flex flex-wrap gap-6">
+        <div className="group w-[calc(50%-12px)]">
+          <Card
+            title="Meta Tag Generator"
+            description="Produce the most beneficial meta tags for your webpage to enhance SEO and enrich the experience for search engine users."
+            link="/meta-tags"
+          />
+        </div>
+      </div>
+    </main>
+  );
 }
