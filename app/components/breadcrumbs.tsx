bb734516941394cb37efa2ca73544c701845b342
farukipekcom <faruk@farukipek.com>
2023-08-28 12:27:27 -0400

    fix: update breadcrumbs

--- app/components/breadcrumbs.tsx
+++ app/components/breadcrumbs.tsx
@@ -4,51 +4,35 @@ import React from "react";
 
 import { usePathname } from "next/navigation";
 interface Props {
-  count: number;
-  page1: string;
-  page2?: string;
+  items: {
+    name: string;
+    link?: string;
+  }[];
 }
 
 export default function Breadcrumbs(Props: Props) {
-  const { count, page1 = "", page2 = "" } = Props;
+  const { items } = Props;
   const pathname = usePathname();
-  const pathList = pathname.split("/");
+  console.log("XX", pathname);
+
   return (
     <div className="flex items-center gap-x-2 text-sm font-medium text-secondary dark:text-white">
-      <Link href="/">Home</Link>
-      {page1.length > 0 && count === 1 ? (
-        <>
-          <span className="text-gray dark:text-gray">{">"}</span>
-          <Link
-            href={pathList[1]}
-            className="rounded-small bg-green_light px-2 py-1 text-primary dark:text-secondary"
-          >
-            {page1}
-          </Link>
-        </>
-      ) : (
-        ""
-      )}
-      {page2.length > 0 && count === 2 ? (
-        <>
-          <span>{">"}</span>
-          <Link
-            href={`../` + pathList[1]}
-            className="text-primary dark:text-secondary"
-          >
-            {page1}
-          </Link>
-          <span>{">"}</span>
-          <Link
-            href={`../` + pathList[1] + "/" + pathList[2]}
-            className="text-primary dark:text-secondary"
-          >
-            {page2}
-          </Link>
-        </>
-      ) : (
-        ""
-      )}
+      {items.map((item: any, index: number) => {
+        return (
+          <div className="afterarrow text-primary dark:text-white">
+            <Link
+              href={`https://generateforfrontend.com` + item.link}
+              className={`${
+                item.link === pathname
+                  ? "rounded-small bg-green_light px-2 py-1 dark:text-primary"
+                  : ""
+              } `}
+            >
+              {item.name}
+            </Link>
+          </div>
+        );
+      })}
     </div>
   );
 }
--- app/meta-tag-generator/content.tsx
+++ app/meta-tag-generator/content.tsx
@@ -20,51 +20,34 @@ export default function Content() {
     viewport: true,
     robots: robots[0],
   });
-  const text = [
-    `<title>${form.title}</title>\n`,
-    `<meta name="description" content="${form.description}">\n`,
-    `<meta name="author" content="${form.author}">\n`,
-    `<meta charset="${form.charset}">\n`,
-    `<meta name="robots" content="${form.robots}">\n`,
-    `<meta name="viewport" content="width=device-width, initial-scale=1">\n`,
-  ];
   const handleChange = (event: any) => {
     setForm({ ...form, [event.target.name]: event.target.value });
   };
   const handleChangeCheckbox = (event: any) => {
     setForm({ ...form, [event.target.name]: event.target.checked });
   };
-  const data = `${form.title && text[0]}${form.description && text[1]}${
-    form.author && text[2]
-  }${form.charset && text[3]}${form.robots && text[4]}${
-    form.viewport === true ? text[5] : ""
+  const data = `${form.title && `<title>${form.title}</title>\n`}${
+    form.description &&
+    `<meta name="description" content="${form.description}">\n`
+  }${form.author && `<meta name="author" content="${form.author}">\n`}${
+    form.charset && `<meta charset="${form.charset}">\n`
+  }${form.robots && `<meta name="robots" content="${form.robots}">\n`}${
+    form.viewport === true
+      ? `<meta name="viewport" content="width=device-width, initial-scale=1">\n`
+      : ""
   }`;
-  console.log(
-    Jsonld({
-      title: "Faruk",
-      description: "Aciklama",
-      datePublished: "2023-08-24",
-      dateModified: "2023-08-24",
-      link: "https://generateforfrontend.com/meta-tag-generator",
-      breadcrumb: [
-        {
-          position: 1,
-          name: "Home",
-          item: "https://generateforfrontend.com/",
-        },
-        {
-          position: 2,
-          name: "Meta Tag Generator",
-          item: "https://generateforfrontend.com/meta-tag-generator",
-        },
-      ],
-    }),
-  );
-
   return (
     <>
       <div className="xl:pr- md:w-full lg:w-full xl:w-1/2 xl:border-r xl:border-solid xl:border-borderLight xl:dark:border-border">
-        <Breadcrumbs count={1} page1="Meta Tags" page2="Ikinci" />
+        <Breadcrumbs
+          items={[
+            { name: "Home", link: "/" },
+            {
+              name: "Deneme Tags",
+              link: "/meta-tag-generator",
+            },
+          ]}
+        />
         <Title title="Meta Tags Generator" />
         <Description description="A meta tags code generator is a tool that helps you create and manage the meta tags for your website. Meta tags are HTML tags that provide information about your website to search engines and other web browsers. </br></br>They can be used to improve the visibility of your website in search results, and to provide additional information about your website to visitors." />
         <div className="mt-9 flex flex-col gap-y-6">
