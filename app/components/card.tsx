470ee0cd4b7e60f05e9db1fcf5c7aa24b2cb8270
farukipekcom <faruk@farukipek.com>
2023-08-30 20:43:35 -0400

    styles: update card design

--- app/components/card.tsx
+++ app/components/card.tsx
@@ -16,10 +16,10 @@ export default function Card(Props: Props) {
         <span className="text-lg font-bold leading-none text-primary">
           {id}
         </span>
-        <h3 className="mt-4 text-[32px] font-bold leading-[38px] text-primary sm:h-12 lg:h-20 lg:w-[250px] ">
+        <h3 className="mt-4 text-[32px] font-bold leading-[38px] text-primary sm:h-12 lg:h-20 lg:w-[250px] lg:text-[28px] xl:w-[340px] xl:text-[32px] 2xl:w-full">
           {title}
         </h3>
-        <p className="mt-6 text-base font-medium text-secondary">
+        <p className="mt-6 flex h-12 items-end text-base font-medium text-secondary">
           {description}
         </p>
         <div className="absolute right-6 top-6 z-10 flex h-12 w-12 items-center justify-center rounded-full bg-white transition-colors duration-300 ease-in-out group-hover:bg-primary">
